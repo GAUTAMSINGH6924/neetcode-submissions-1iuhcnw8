d0992cc9b9918202a1bc14442c9d577c00ce59d7
Gautam Singh <88306756+GAUTAMSINGH6924@users.noreply.github.com>
2026-04-19 16:14:20 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(char[][] board, String word,int i,int j,int index){
+        if(i>=board.length || j>=board[0].length || i<0 || j<0){
+            return false;
+        }
+        if(board[i][j] != word.charAt(index)){
+            return false;
+        }
+        if(index==word.length()-1){
+            return true;
+        }
+        char temp=board[i][j];
+        board[i][j]='#';
+
+        boolean found=search(board,word,i+1,j,index+1) ||search(board,word,i,j+1,index+1)||
+        search(board,word,i-1,j,index+1)||search(board,word,i,j-1,index+1);
+        board[i][j]=temp;
+        return found;
+    }
+    public boolean exist(char[][] board, String word) {
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(word.charAt(0)==board[i][j]){
+                    if(search(board,word,i,j,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}

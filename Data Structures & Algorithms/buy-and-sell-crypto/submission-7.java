42eb1cd09b68f9b0dc86b402137626fe08ac9b98
Gautam Singh <88306756+GAUTAMSINGH6924@users.noreply.github.com>
2026-05-11 23:55:34 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minVal=Integer.MAX_VALUE;
+        int maxProfit=Integer.MIN_VALUE;
+
+        for(int i=0;i<prices.length;i++){
+            minVal=Math.min(minVal,prices[i]);
+            maxProfit=Math.max(maxProfit,prices[i]-minVal);
+        }
+        return maxProfit;
+    }
+}

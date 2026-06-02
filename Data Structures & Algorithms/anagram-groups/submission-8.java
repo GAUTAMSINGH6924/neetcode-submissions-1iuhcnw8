753441fca7eeabb1b66de843824e78c48e4cf9c7
Gautam Singh <88306756+GAUTAMSINGH6924@users.noreply.github.com>
2026-06-02 10:10:50 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>map=new HashMap<>();
+
+        for(String word:strs){
+            char[]str=word.toCharArray();
+            Arrays.sort(str);
+
+            String key=new String(str);
+
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}

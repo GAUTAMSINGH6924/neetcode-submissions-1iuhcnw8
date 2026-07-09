07dbc58102d2e0700c49aa77c24d0b681a2b9745
Gautam Singh <88306756+GAUTAMSINGH6924@users.noreply.github.com>
2026-07-10 00:13:46 +0530

    Add: unique-paths-ii - submission-0

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int helper(int[][] obstacleGrid, int m, int n, int[][] dp) {
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+        if (obstacleGrid[m][n] == 1) {
+            return 0;
+        }
+        if (m == 0 && n == 0) {
+            return 1;
+        }
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
+        int top = helper(obstacleGrid, m - 1, n, dp);
+        int left = helper(obstacleGrid, m, n - 1, dp);
+
+        return dp[m][n] = top + left;
+    }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return helper(obstacleGrid, m - 1, n - 1, dp);
+    }
+}

81f82411b5eb036f419dae1c462cc78acdd90b78
Gautam Singh <88306756+GAUTAMSINGH6924@users.noreply.github.com>
2026-08-27 14:50:11 +0530

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int helper(int[] nums, Integer[] dp, int idx) {
+        if (idx < 0) {
+            return 0;
+        }
+
+        if (dp[idx] != null) {
+            return dp[idx];
+        }
+        int notTake = helper(nums, dp, idx - 1);
+
+        int take = nums[idx] + helper(nums, dp, idx - 2);
+        return dp[idx] = Math.max(take, notTake);
+    }
+    public int rob(int[] nums) {
+        Integer[] dp = new Integer[nums.length];
+
+        return helper(nums, dp, nums.length - 1);
+    }
+}
